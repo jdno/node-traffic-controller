35c6b42fd7321751d232124a84965a8f20d0dd10
Jan David <jdno@jdno.dev>
2022-04-21 11:59:30 +0200

    Add the game's Node SDK
    The game's SDK for the Node ecosystem has been added as a dependency,
    and the example from the game's repository [^1] has been copied into the
    project as a proof-of-concept.
    
    [^1]: https://github.com/jdno/auto-traffic-control

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,41 @@
+import {
+  getCredentials,
+  AtcServiceClient,
+  GetVersionRequest,
+  GetVersionResponse,
+} from "auto-traffic-control";
+
 function main() {
-  console.log("Hello, World!");
+  const atcService = new AtcServiceClient("localhost:4747", getCredentials());
+
+  atcService.getVersion(
+    new GetVersionRequest(),
+    (err, response: GetVersionResponse) => {
+      if (err != null) {
+        throw err;
+      }
+
+      const version = response.getVersion();
+
+      if (version != null) {
+        let versionString = [
+          version.getMajor(),
+          version.getMinor(),
+          version.getPatch(),
+        ].join(".");
+
+        if (version.getPre() !== "") {
+          versionString = versionString.concat(version.getPre());
+        }
+
+        console.log(
+          `Auto Traffic Control is running version '${versionString}'`
+        );
+      } else {
+        throw new Error("Requesting the version returned an empty response.");
+      }
+    }
+  );
 }
 
 main();
