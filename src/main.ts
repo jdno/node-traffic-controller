642ece25ed2205286fd1de2f174c835fb29d9dde
Jan David <jdno@jdno.dev>
2022-04-21 12:28:23 +0200

    Start a new game
    The program has been rewritten to no longer request the version of the
    game, but instead start a new game.

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,20 @@
 import {
   getCredentials,
-  AtcServiceClient,
-  GetVersionRequest,
-  GetVersionResponse,
+  GameServiceClient,
+  StartGameRequest,
+  StartGameResponse,
 } from "auto-traffic-control";
 
 function main() {
-  const atcService = new AtcServiceClient("localhost:4747", getCredentials());
+  const gameService = new GameServiceClient("localhost:4747", getCredentials());
 
-  atcService.getVersion(
-    new GetVersionRequest(),
-    (err, response: GetVersionResponse) => {
-      if (err != null) {
-        throw err;
-      }
-
-      const version = response.getVersion();
-
-      if (version != null) {
-        let versionString = [
-          version.getMajor(),
-          version.getMinor(),
-          version.getPatch(),
-        ].join(".");
-
-        if (version.getPre() !== "") {
-          versionString = versionString.concat(version.getPre());
-        }
-
-        console.log(
-          `Auto Traffic Control is running version '${versionString}'`
-        );
-      } else {
-        throw new Error("Requesting the version returned an empty response.");
-      }
+  gameService.startGame(new StartGameRequest(), (err) => {
+    if (err != null) {
+      throw err;
     }
-  );
+
+    console.log("Started a new game. Good luck!");
+  });
 }
 
 main();
