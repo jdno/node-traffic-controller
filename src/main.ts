93b590837901c2f7787d5c5d0b55848250c91d0e
Jan David <jdno@jdno.dev>
2022-04-23 10:23:46 +0200

    Subscribe to the event stream
    The program now subscribes to the game's event stream, and prints a
    selection of events to the terminal. It detects when an airplane has
    been spawned, and prints information about the airplane and its flight
    plan. And when the game ends, the score is printed to the terminal
    before the program exits.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,69 @@
 import {
   getCredentials,
+  AirplaneDetected,
+  EventServiceClient,
   GameServiceClient,
+  GameStopped,
   StartGameRequest,
-  StartGameResponse,
+  StreamRequest,
+  StreamResponse,
 } from "auto-traffic-control";
 
 function main() {
+  subscribeToEvents();
+  startGame();
+}
+
+function subscribeToEvents(): void {
+  const eventService = new EventServiceClient(
+    "localhost:4747",
+    getCredentials()
+  );
+
+  const stream = eventService.stream(new StreamRequest());
+
+  stream.on("data", processMessage);
+  stream.on("end", streamClosed);
+}
+
+function processMessage(streamResponse: StreamResponse): void {
+  const airplaneDetected = streamResponse.getAirplaneDetected();
+  if (airplaneDetected != undefined) {
+    updateFlightPlan(airplaneDetected);
+  }
+
+  const gameStopped = streamResponse.getGameStopped();
+  if (gameStopped != undefined) {
+    exit(gameStopped);
+  }
+}
+
+function updateFlightPlan(event: AirplaneDetected): void {
+  const airplane = event.getAirplane();
+  if (airplane == undefined) {
+    throw new Error("Received AirplaneDetected event without an airplane");
+  }
+
+  const id = airplane.getId();
+  const flightPlan = airplane.getFlightPlanList();
+  const nextNode = flightPlan.at(0);
+
+  console.log(`Detected airplane ${id} heading towards ${nextNode}.`);
+}
+
+function exit(event: GameStopped): void {
+  const score = event.getScore();
+
+  console.log(`Game stopped! Score: ${score}`);
+  process.exit();
+}
+
+function streamClosed(): void {
+  console.log("The server closed the event stream.");
+  process.exit(1);
+}
+
+function startGame(): void {
   const gameService = new GameServiceClient("localhost:4747", getCredentials());
 
   gameService.startGame(new StartGameRequest(), (err) => {
